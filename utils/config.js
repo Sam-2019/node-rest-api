d9995652013a2df0102081a504b068b10ffab938
Sam-2019 <samtmartey@gmail.com>
2022-08-20 14:22:58 +0100

    added slack

--- utils/config.js
+++ utils/config.js
@@ -2,10 +2,12 @@ const DB_URI = process.env.DB_URI;
 const NODE_ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT;
 const NIMBLE = process.env.NIMBLE;
+const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK;
 
 module.exports = {
   DB_URI,
   NODE_ENV,
   NIMBLE,
   PORT,
+  SLACK_WEBHOOK
 };
