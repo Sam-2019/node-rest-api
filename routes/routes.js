f72db0fd53efa11153d4c770c929fd4d515a8fed
Sam-2019 <samtmartey@gmail.com>
2022-08-20 10:27:35 +0100

    route update

--- routes/routes.js
+++ routes/routes.js
@@ -2,27 +2,12 @@ const express = require("express");
 const Numbers = require("../db/numbers");
 const router = express.Router();
 
-//Post Method
-// router.post('/post', async (req, res) => {
-//     const data = new Numbers({
-//         name: req.body.name,
-//         age: req.body.age
-//     })
-
-//     try {
-//         const dataToSave = await data.save();
-//         res.status(200).json(dataToSave)
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// })
-
 //Get all Method
 router.get("/get_names", async (req, res) => {
   try {
-    const data = await Numbers.find();
-    res.json(data);
+    // const data = await Numbers.find();
+    // res.json(data);
+    res.json("Konichiwa");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -31,29 +16,12 @@ router.get("/get_names", async (req, res) => {
 // Get by ID Method
 router.get("/gwt_name/:id", async (req, res) => {
   try {
-    const data = await Numbers.findOne({ phone: req.params.id });
-    res.json(data);
+    // const data = await Numbers.findOne({ phone: req.params.id });
+    // res.json(data);
+    res.json("Konichiwa");
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
-
-//Update by ID Method
-// router.patch('/update/:id', async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const updatedData = req.body;
-//         const options = { new: true };
-
-//         const result = await Numbers.findByIdAndUpdate(
-//             id, updatedData, options
-//         )
-
-//         res.send(result)
-//     }
-//     catch (error) {
-//         res.status(500).json({ message: error.message })
-//     }
-// })
 
 module.exports = router;
