6718999cd4fb7075c236f3b8f6860d49bff6580b
Sam-2019 <samtmartey@gmail.com>
2022-08-20 03:39:31 +0100

    added config

---
+++ utils/config.js
@@ -0,0 +1,11 @@
+const DB_URI = process.env.DB_URI;
+const NODE_ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT;
+const NIMBLE = process.env.NIMBLE;
+
+module.exports = {
+  DB_URI,
+  NODE_ENV,
+  NIMBLE,
+  PORT,
+};
