6aa28c7a30c78b7ca846bf652f3ff1a0965b03cd
Sam-2019 <samtmartey@gmail.com>
2022-08-20 15:26:35 +0100

    updated ping

--- utils/ping.js
+++ utils/ping.js
@@ -2,25 +2,27 @@ const axios = require("axios");
 const dataCount = require("./slack");
 
 function ping() {
-  axios
-    .get("https://helliomessaging.com")
-    .then((res) => {
-      if (res.status != 200) {
-        dataCount("HelioSMS", "server is down", 0);
-        return;
-      }
+  setInterval(async function () {
+    axios
+      .get("https://helliomessaging.com")
+      .then((res) => {
+        if (res.status != 200) {
+          dataCount("HelioSMS", "server is down", 0);
+          return;
+        }
 
-      const info = res.data;
-      const find = info.includes("Hellio provides an API-first Caa");
+        const info = res.data;
+        const find = info.includes("Hellio provides an API-first Caa");
 
-      if (find === false) {
-        dataCount("HelioSMS", "server is down", 0);
-        return;
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+        if (find === false) {
+          dataCount("HelioSMS", "server is down", 0);
+          return;
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, 1800000);
 }
 
 ping();
