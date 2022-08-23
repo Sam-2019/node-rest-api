7b437f625e5ce7102f07fbe39fb70701f7aab905
Sam-2019 <samtmartey@gmail.com>
2022-08-23 22:59:28 +0100

    added helio

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,13 @@
 const MESSAGE =
   "Could not resolve account name. Check parameters or try again.";
 const ID = "28";
+const HELIOSMS1 = "⚠ ⚠ ⚠ Issues With Message Delivery ⚠ ⚠ ⚠";
+const HELIOSMS2 =
+  "Our apologies for the issues currently being faced with delivery of messages";
 
 module.exports = {
   MESSAGE,
   ID,
+  HELIOSMS1,
+  HELIOSMS2,
 };
