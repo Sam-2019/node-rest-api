72e131cb60eb87b7099e83772d7fc6ad27b3ae5c
Sam-2019 <samtmartey@gmail.com>
2022-08-23 23:00:18 +0100

    updated png

--- utils/ping.js
+++ utils/ping.js
@@ -1,27 +1,36 @@
 const axios = require("axios");
-const slackNotify = require("./slack");
+const { slackNotify } = require("./slack");
 const { SET_INTERVAL } = require("./config");
+const { HELIOSMS1 } = require("./constants");
 
 function ping() {
   setInterval(async function () {
     axios
       .get("https://helliomessaging.com")
       .then((res) => {
-        if (res.status != 200) {
-          slackNotify("HelioSMS", "server is down", 0);
-          return;
-        }
+        console.log({ res: res.status });
+        // if (res.status != 200) {
+        slackNotify("HelioSMS", "server is up");
+        console.log("HelioSMS: server is up");
+        //   return;
+        // }
 
-        const info = res.data;
-        const find = info.includes("Hellio provides an API-first Caa");
+        // const info = res.data;
+        // const find = info.includes("Hellio provides an API-first Caa");
 
-        if (find === false) {
-          slackNotify("HelioSMS", "server is down", 0);
-          return;
-        }
+        // if (find === false) {
+        //   slackNotify("HelioSMS", "server is down", 0);
+        //   return;
+        // }
       })
       .catch((error) => {
-        console.error(error);
+        // console.error(error);
+        const deliveryIssues = error.includes(HELIOSMS1);
+
+        if (deliveryIssues === true) {
+          slackNotify("HelioSMS", "server is down");
+        }
+        console.log("HelioSMS: server is down");
       });
   }, SET_INTERVAL);
 }
