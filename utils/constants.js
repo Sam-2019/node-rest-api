d75df1d3355fc69e4b33930abd72095bc547a793
Sam-2019 <samtmartey@gmail.com>
2022-08-24 22:19:13 +0100

    updated constants

--- utils/constants.js
+++ utils/constants.js
@@ -4,10 +4,16 @@ const ID = "28";
 const HELIOSMS1 = "⚠ ⚠ ⚠ Issues With Message Delivery ⚠ ⚠ ⚠";
 const HELIOSMS2 =
   "Our apologies for the issues currently being faced with delivery of messages";
+const HELIOSDOWN = "HellioSMS: server is down";
+const HELIOSUP = "HellioSMS: server is up";
+const HELIOSURL = "https://helliomessaging.com"
 
 module.exports = {
   MESSAGE,
   ID,
   HELIOSMS1,
   HELIOSMS2,
+  HELIOSDOWN,
+  HELIOSUP,
+  HELIOSURL
 };
