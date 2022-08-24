08d370d380d926aad18296ce0bc1a177842894e7
Sam-2019 <samtmartey@gmail.com>
2022-08-24 22:21:32 +0100

    imported repositories

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const Numbers = require("../db/numbers");
+const { getOne, getAll } = require("../db/repositories");
 const router = express.Router();
 
 //Get all Method
 router.get("/get_names", async (req, res) => {
   try {
-    // const data = await Numbers.find();
+    // const data = await getAll();
     // res.json(data);
     res.json("Konnichiwa");
   } catch (error) {
@@ -16,7 +16,7 @@ router.get("/get_names", async (req, res) => {
 // Get by ID Method
 router.get("/get_name/:id", async (req, res) => {
   try {
-    // const data = await Numbers.findOne({ phone: req.params.id });
+    // const data = await getOne(req.params.id);
     // res.json(data);
     res.json("Konnichiwa");
   } catch (error) {
