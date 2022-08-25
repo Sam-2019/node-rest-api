771ac5e225bc553be66d8d439eb20c6657640ec0
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2022-08-25 08:33:22 +0000

    added networkCodes

--- utils/constants.js
+++ utils/constants.js
@@ -7,6 +7,15 @@ const HELIOSMS2 =
 const HELIOSDOWN = "HellioSMS: server is down";
 const HELIOSUP = "HellioSMS: server is up";
 const HELIOSURL = "https://helliomessaging.com"
+// const prefixMTN = [024, 054, 055, 059, 025];
+// const prefixVODAFONE = [020, 050];
+// const networkcodeAIRRTELTIGO = [027, 057, 026, 056];
+
+const networkCodes = [
+  {mtn: [024, 054, 055, 059, 025]},
+  {vodefone: [020, 050]},
+  {airteltigo: [027, 057, 026, 056]}
+]
 
 module.exports = {
   MESSAGE,
