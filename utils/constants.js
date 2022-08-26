57e5b8f2802a4dd69e59f986798732c7ec6f0018
Sam-2019 <samtmartey@gmail.com>
2022-08-27 00:19:12 +0100

    aded funcs

--- utils/constants.js
+++ utils/constants.js
@@ -6,46 +6,56 @@ const HELIOSMS2 =
   "Our apologies for the issues currently being faced with delivery of messages";
 const HELIOSDOWN = "HellioSMS: server is down";
 const HELIOSUP = "HellioSMS: server is up";
-const HELIOSURL = "https://helliomessaging.com"
-// const prefixMTN = [024, 054, 055, 059, 025];
-// const prefixVODAFONE = [020, 050];
-// const networkcodeAIRRTELTIGO = [027, 057, 026, 056];
+const HELIOSURL = "https://helliomessaging.com";
 
 function sliceNumber(data) {
- if (!data) return "No data!";
- return data.slice(0, 3);
+  if (!data) return "No data!";
+  return data.slice(0, 3);
 }
 
 const networkCodes = [
- { mtn: ["024", "054", "055", "059", "025"] },
- { vodafone: ["020", "050"] },
- { airteltigo: ["027", "057", "026", "056"] },
+  { mtn: ["024", "054", "055", "059", "025"] },
+  { vodafone: ["020", "050"] },
+  { airteltigo: ["027", "057", "026", "056"] },
 ];
 
-const bank_ids = { mtn: 28, vodafone: 66, airteltigo: 29 };
+const bank_ids = {
+  mtn: {
+    id: 28,
+    code: "MTN",
+  },
+  vodafone: {
+    id: 66,
+    code: "VOD",
+  },
+  airteltigo: {
+    id: 29,
+    code: "ATL",
+  },
+};
+
 function getData(data) {
- if (!data) {
-  return "No data";
- }
+  if (!data) {
+    return "No data";
+  }
 
- if (networkCodes[0].mtn.includes(sliceNumber(data))) {
-  return bank_ids.mtn;
- }
+  if (networkCodes[0].mtn.includes(sliceNumber(data))) {
+    return bank_ids.mtn.code;
+  }
 
- if (networkCodes[1].vodafone.includes(sliceNumber(data))) {
-  return bank_ids.vodafone;
- }
+  if (networkCodes[1].vodafone.includes(sliceNumber(data))) {
+    return bank_ids.vodafone.code;
+  }
 
- if (networkCodes[2].airteltigo.includes(sliceNumber(data))) {
-  return bank_ids.airteltigo;
- }
+  if (networkCodes[2].airteltigo.includes(sliceNumber(data))) {
+    return bank_ids.airteltigo.code;
+  }
 
- return;
+  return;
 }
 
 // const info = getData(number);
 // console.log({ info });
-
 
 module.exports = {
   MESSAGE,
@@ -54,5 +64,6 @@ module.exports = {
   HELIOSMS2,
   HELIOSDOWN,
   HELIOSUP,
-  HELIOSURL
+  HELIOSURL,
+  getData,
 };
