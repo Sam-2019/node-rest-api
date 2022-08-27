2bb40673ab04884d05fad40be3870965315a5f6b
Sam-2019 <samtmartey@gmail.com>
2022-08-27 19:21:28 +0100

    added cleanup

---
+++ utils/dbCleanup.js
@@ -0,0 +1,20 @@
+const { shuffle } = require("../utils/runner");
+const { getFailedWithIDS, updateMany } = require("../db/repositories");
+
+const bankIDs = ["28", "29", "66"];
+
+async function cleanup() {
+  setInterval(async function () {
+    shuffle(bankIDs);
+
+    const results = await getFailedWithIDS(bankIDs[0]);
+    console.log(results);
+    if (!results) return;
+
+    await updateMany(bankIDs[0]);
+  }, 10000);
+}
+
+module.exports = {
+  cleanup,
+};
