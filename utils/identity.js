4877a118ec5baf9941102c97bab497f3579ffb2e
Sam-2019 <samtmartey@gmail.com>
2022-09-17 19:04:41 +0100

    added identity

---
+++ utils/identity.js
@@ -0,0 +1,36 @@
+const paystack = require("paystack-api")(process.env.NIMBLE);
+const truecallerjs = require("truecallerjs");
+const { TRUECALLER } = require("../utils/config");
+
+async function stack(phone, accountCode, res) {
+  try {
+    const paystackData = await paystack.verification.resolveAccount({
+      account_number: phone,
+      bank_code: accountCode,
+    });
+
+    return paystackData;
+  } catch (error) {
+    // res.status(500).json({ message: error.message });
+  }
+}
+
+async function caller(pn, res) {
+  try {
+    var truecallerData = truecallerjs.searchNumber({
+      number: pn.getNumber(),
+      countryCode: pn.getRegionCode(),
+      installationId: TRUECALLER,
+      output: "JSON",
+    });
+
+    return truecallerData;
+  } catch (error) {
+    // res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = {
+  stack,
+  caller,
+};
