e1d6a284b536b44c44ab286200337e7f41f053e8
Sam-2019 <samtmartey@gmail.com>
2022-09-18 19:16:35 +0100

    code revert

--- routes/routes.js
+++ routes/routes.js
@@ -29,33 +29,33 @@ router.get("/get_name/:id", async (req, res) => {
 
 router.get("/id/:id", async (req, res) => {
   // console.log(req.params.id);
-  res.json(`${req.params.id}`);
-  // let countryCode = "GH";
-  // const number = req.params.id;
-  // const pn = parsePhoneNumber(number, countryCode);
-  // const phone = pn.getNumber("significant"); // -> '707123456'
-  // const updated = `0${phone}`;
-  // const accountCode = getData(updated);
+  // res.json(`${req.params.id}`);
+  let countryCode = "GH";
+  const number = req.params.id;
+  const pn = parsePhoneNumber(number, countryCode);
+  const phone = pn.getNumber("significant"); // -> '707123456'
+  const updated = `0${phone}`;
+  const accountCode = getData(updated);
 
-  // const paystack = await stack(updated, accountCode, res);
-  // const truecaller = await caller(pn, res);
+  const paystack = await stack(updated, accountCode, res);
+  const truecaller = await caller(pn, res);
 
-  // if (truecaller && !paystack) {
-  //   // return res.json([{ truecaller: truecaller }, { paystack: null }]);
-  //   return [{ truecaller: truecaller }, { paystack: null }];
-  // }
+  if (truecaller && !paystack) {
+    // return res.json([{ truecaller: truecaller }, { paystack: null }]);
+    return [{ truecaller: truecaller }, { paystack: null }];
+  }
 
-  // if (paystack && !truecaller) {
-  //   // return res.json([{ paystack: paystack }, { truecaller: null }]);
-  //   return [{ paystack: paystack }, { truecaller: null }];
-  // }
+  if (paystack && !truecaller) {
+    // return res.json([{ paystack: paystack }, { truecaller: null }]);
+    return [{ paystack: paystack }, { truecaller: null }];
+  }
 
-  // if (!paystack && !truecaller) {
-  //   // return res.json([{ truecaller: truecaller }, { paystack: null }]);
-  //   return [{ truecaller: truecaller }, { paystack: nul }];
-  // }
+  if (!paystack && !truecaller) {
+    // return res.json([{ truecaller: truecaller }, { paystack: null }]);
+    return [{ truecaller: truecaller }, { paystack: nul }];
+  }
 
-  // // return res.json([{ paystack: paystack }, { truecaller: truecaller }]);
-  // return [{ paystack: paystack }, { truecaller: truecaller }];
+  // return res.json([{ paystack: paystack }, { truecaller: truecaller }]);
+  return [{ paystack: paystack }, { truecaller: truecaller }];
 });
 module.exports = router;
