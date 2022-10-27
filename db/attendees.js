8f7673f787773ccf2c04a9cd4f5787c1edcd6034
Sam-2019 <samtmartey@gmail.com>
2022-10-27 20:07:48 +0100

    rm redundant code

--- db/attendees.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-const { dataSchema } = require("./models/attendees");
-
-module.exports = mongoose.model("Attendees", dataSchema);
--- db/numbers.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose");
-const { dataSchema } = require("./models/numbers");
-
-module.exports = mongoose.model("Numbers", dataSchema);
