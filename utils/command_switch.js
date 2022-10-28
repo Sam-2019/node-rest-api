03bb2a2ab7b42298501bde820d58d0fe8f028a78
Sam-2019 <samtmartey@gmail.com>
2022-10-28 21:53:38 +0100

    added command switch

---
+++ utils/command_switch.js
@@ -0,0 +1,39 @@
+const { getSaved, getFailed, getRemaining } = require("../db/repository");
+const { pingHellio } = require("./ping");
+
+const getInfo = async (command) => {
+  let model;
+  let text;
+
+  switch (command) {
+    case "saved":
+      model = await getSaved();
+      text = "Saved";
+      break;
+    case "failed":
+      model = await getFailed();
+      text = "Failed";
+      break;
+    case "rawIDs":
+      model = await getRemaining();
+      text = "Untouched";
+      break;
+    case "hellio":
+      model = await pingHellio();
+      text = "";
+      break;
+
+    default:
+      model = null;
+      text = "app pinged";
+  }
+
+  return {
+    model,
+    text,
+  };
+};
+
+module.exports = {
+  getInfo,
+};
