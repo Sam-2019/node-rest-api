93a1819d8ce4a6c0e4fe58f6c25cb93500facf3e
Sam-2019 <samtmartey@gmail.com>
2022-11-05 20:14:26 +0000

    updated ping

--- utils/ping.js
+++ utils/ping.js
@@ -4,28 +4,20 @@ const { SET_INTERVAL } = require("./config");
 const { HELIOSDOWN, HELIOSUP, HELIOSURL, HELIOSMS } = require("./constants");
 
 async function pingHellio() {
-  try {
-    const response = await axios.get(HELIOSURL);
-    if (response) {
-      return HELIOSUP;
-    }
-  } catch (error) {
-    return HELIOSDOWN;
-  }
+  axios
+    .get(HELIOSURL)
+    .then(() => {
+      console.log(HELIOSUP);
+    })
+    .catch((error) => {
+      console.log(HELIOSDOWN);
+      slackNotify(HELIOSMS, error);
+    });
 }
 
 function ping() {
   setInterval(async function () {
-    axios
-      .get(HELIOSURL)
-      .then((res) => {
-        // console.log({ res: res.status });
-        console.log(HELIOSUP);
-      })
-      .catch((error) => {
-        console.log(HELIOSDOWN);
-        slackNotify(HELIOSMS, error);
-      });
+    pingHellio();
   }, SET_INTERVAL);
 }
 
