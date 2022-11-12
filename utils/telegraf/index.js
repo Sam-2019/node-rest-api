1ce17c3fd2ed4598bc3dc029671489470cd613b0
Sam-2019 <samtmartey@gmail.com>
2022-11-12 09:28:29 +0000

    rmved run_command

--- utils/telegraf/index.js
+++ utils/telegraf/index.js
@@ -1,25 +1,6 @@
 const { Telegraf } = require("telegraf");
 const { TOKEN } = require("../config");
-const { getInfo } = require("./command_switch");
 const bot = new Telegraf(TOKEN);
-
-async function run_comand(command) {
-  let { model, text } = await getInfo(command);
-
-  bot.command(command, async (ctx) => {
-    if (model === null) {
-      return ctx.reply(text);
-    }
-
-    ctx.reply(`${text} ${model}`);
-  });
-}
-
-run_comand("ping");
-run_comand("saved");
-run_comand("failed");
-run_comand("rawIDs");
-run_comand("hellio");
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
