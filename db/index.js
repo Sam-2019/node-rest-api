22ce9a411ff0386ef9dc301dd05a54607935aff7
Sam-2019 <samtmartey@gmail.com>
2022-11-26 10:51:55 +0000

    db index update

--- db/index.js
+++ db/index.js
@@ -3,13 +3,13 @@ const { DB_URI } = require("../utils/config");
 
 const mongoURI = DB_URI;
 
+var dbConn = mongoose.connection;
+dbConn.on("connected", function () {
+  console.log("Mongoose connected");
+});
+
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   autoIndex: false,
-});
-
-var dbConn = mongoose.connection;
-dbConn.on("connected", function () {
-  console.log("Mongoose connected");
 });
