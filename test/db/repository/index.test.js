312f3174f5ac0901733588f0ec312048325be4b5
Sam-2019 <samtmartey@gmail.com>
2022-12-06 22:46:50 +0000

    repository test (w-i-p)

--- test/db/repository/index.test.js
+++ test/db/repository/index.test.js
@@ -1,4 +1,16 @@
 const axios = require("axios");
+const {
+  getSaved,
+  getFailed,
+  getTimeout,
+  getDataIDS,
+  getFailedIDS,
+  getMomoStatus,
+  getRemaining,
+  getAll,
+  getOne,
+  getFailedWithIDS,
+} = require("../../../db/repository");
 const users = [{ name: "Bob" }];
 
 jest.mock("axios");
@@ -12,4 +24,54 @@ test("should fetch users", () => {
 
   //   console.log(users);
   //   expect(users).toContain("Bob");
+});
+
+test("should count saved", async () => {
+  let result = await getSaved();
+  expect(result).toBeUndefined();
+});
+
+test("should count failed", async () => {
+  let result = await getFailed();
+  expect(result).toBeUndefined();
+});
+
+test("should count timeout", async () => {
+  let result = await getTimeout();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch data with IDs", async () => {
+  let result = await getDataIDS();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch get failed IDs", async () => {
+  let result = await getFailedIDS();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch get momo status", async () => {
+  let result = await getMomoStatus();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch get remaining", async () => {
+  let result = await getRemaining();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch get all records", async () => {
+  let result = await getAll();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch get one record", async () => {
+  let result = await getOne();
+  expect(result).toBeUndefined();
+});
+
+test("should fetch get failed with IDS", async () => {
+  let result = await getFailedWithIDS();
+  expect(result).toBeUndefined();
 });
