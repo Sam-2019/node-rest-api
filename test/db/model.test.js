9ad39b626b05d394264d8380b9ceceef96c87acc
Sam-2019 <samtmartey@gmail.com>
2022-12-07 06:22:12 +0000

    added describe block

--- test/db/model.test.js
+++ test/db/model.test.js
@@ -1,29 +1,31 @@
 const { model } = require("../../db/model");
 
-it("should return Attendee model", async () => {
-  const result = model("attendee");
-  expect(result).not.toBeNull();
-  expect(result).toBeDefined();
-  expect(result).toBeTruthy();
-});
+describe("Model test", () => {
+  it("should return Attendee model", async () => {
+    const result = model("attendee");
+    expect(result).not.toBeNull();
+    expect(result).toBeDefined();
+    expect(result).toBeTruthy();
+  });
 
-it("should return Numbers model", async () => {
-  const result = model("number");
-  expect(result).not.toBeNull();
-  expect(result).toBeDefined();
-  expect(result).toBeTruthy();
-});
+  it("should return Numbers model", async () => {
+    const result = model("number");
+    expect(result).not.toBeNull();
+    expect(result).toBeDefined();
+    expect(result).toBeTruthy();
+  });
 
-it("should return Identity model", async () => {
-  const result = model("identity");
-  expect(result).not.toBeNull();
-  expect(result).toBeDefined();
-  expect(result).toBeTruthy();
-});
+  it("should return Identity model", async () => {
+    const result = model("identity");
+    expect(result).not.toBeNull();
+    expect(result).toBeDefined();
+    expect(result).toBeTruthy();
+  });
 
-it("should return empty model", async () => {
-  const result = model();
-  expect(result).toBeNull();
-  expect(result).toBeDefined();
-  expect(result).toBeFalsy();
+  it("should return empty model", async () => {
+    const result = model();
+    expect(result).toBeNull();
+    expect(result).toBeDefined();
+    expect(result).toBeFalsy();
+  });
 });
