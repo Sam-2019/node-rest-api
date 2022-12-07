8732b93bae8654a8b4b517e99d5892cdee4b4972
Sam-2019 <samtmartey@gmail.com>
2022-12-07 06:21:54 +0000

    rnamed mock data

--- test/db/connection.test.js
+++ test/db/connection.test.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { ACTIVE_MODEL } = require("../../utils/config");
 const { model } = require("../../db/model");
 const Model = model(ACTIVE_MODEL);
-const { data } = require("./mock_data");
+const { mock_data } = require("../../utils/mock_mock_data");
 const { connectDB, disconnectDB } = require("../../db");
 
 describe("Connection", () => {
@@ -15,14 +15,14 @@ describe("Connection", () => {
   });
 
   test("Retrieve attendee by Id", async () => {
-    const attendee = await Model.findById(data.id);
-    expect(attendee.id).toBe(data.id);
-    expect(attendee.mobile_number).toBe(data.mobile_number);
-    expect(attendee.number).toBe(data.number);
+    const attendee = await Model.findById(mock_data.id);
+    expect(attendee.id).toBe(mock_data.id);
+    expect(attendee.mobile_number).toBe(mock_data.mobile_number);
+    expect(attendee.number).toBe(mock_data.number);
     expect(attendee.message).toBeNull();
-    expect(attendee.account_number).toBe(data.account_number);
-    expect(attendee.bank_id).toBe(data.bank_id);
+    expect(attendee.account_number).toBe(mock_data.account_number);
+    expect(attendee.bank_id).toBe(mock_data.bank_id);
     expect(attendee.is_momo_active).toBeTruthy();
-    expect(attendee.name).toBe(data.name);
+    expect(attendee.name).toBe(mock_data.name);
   });
 });
