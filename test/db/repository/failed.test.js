84b07778feb5091bfe4e15ec4872824920229a0a
Sam-2019 <samtmartey@gmail.com>
2022-12-09 06:32:31 +0000

    lint

--- test/db/repository/failed.test.js
+++ test/db/repository/failed.test.js
@@ -13,42 +13,42 @@ const {
 
 describe("On db connection failure", () => {
   test("count saved", async () => {
-    return getSaved().catch(e => expect(e).toMatch('error'));
+    return getSaved().catch((e) => expect(e).toMatch("error"));
   });
 
   test("count failed", async () => {
-    return getFailed().catch(e => expect(e).toMatch('error'));
+    return getFailed().catch((e) => expect(e).toMatch("error"));
   });
 
   test("count timeout", async () => {
-    return getTimeout().catch(e => expect(e).toMatch('error'));
+    return getTimeout().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch data with IDs", async () => {
-    return getDataIDS().catch(e => expect(e).toMatch('error'));
+    return getDataIDS().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch get failed IDs", async () => {
-    return getFailedIDS().catch(e => expect(e).toMatch('error'));
+    return getFailedIDS().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch get momo status", async () => {
-    return getMomoStatus().catch(e => expect(e).toMatch('error'));
+    return getMomoStatus().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch get remaining", async () => {
-    return getRemaining().catch(e => expect(e).toMatch('error'));
+    return getRemaining().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch get all records", async () => {
-    return getAll().catch(e => expect(e).toMatch('error'));
+    return getAll().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch get one record", async () => {
-    return getOne().catch(e => expect(e).toMatch('error'));
+    return getOne().catch((e) => expect(e).toMatch("error"));
   });
 
   test("fetch get failed with IDS", async () => {
-    return getFailedWithIDS().catch(e => expect(e).toMatch('error'));
+    return getFailedWithIDS().catch((e) => expect(e).toMatch("error"));
   });
 });
