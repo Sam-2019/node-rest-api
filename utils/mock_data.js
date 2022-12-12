fb9d9b7747280a10feb7ade0dc12de99d567dd53
Sam-2019 <samtmartey@gmail.com>
2022-12-12 22:06:11 +0000

    added mock data

---
+++ utils/mock_data.js
@@ -0,0 +1,13 @@
+const mock_data = {
+  mobile_number: "+233200737172",
+  number: "0200737172",
+  message: null,
+  account_number: "0200737172",
+  bank_id: "66",
+  is_momo_active: true,
+  name: "MIKE CUI",
+};
+
+module.exports = {
+  mock_data,
+};
