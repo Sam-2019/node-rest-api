b618706235ca90df023b5a31b49cce597461b4dc
Sam-2019 <samtmartey@gmail.com>
2022-12-19 04:31:15 +0000

    added test (w-i-p)

---
+++ test/routes/index.test.js
@@ -0,0 +1,9 @@
+describe("Get api endpoints", () => {
+  test("should return getNames", async () => {});
+
+  test("should return getName", async () => {});
+
+  test("should return getSaved", async () => {});
+
+  test("should return getIdentity", async () => {});
+});
